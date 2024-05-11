0bab0863de4ff4c4c935574edf511ed1070c51c8
olleholt <olivialeighholt97@gmail.com>
2024-05-11 05:41:01 +0000

    I really dont know

---
+++ week5/.ipynb_checkpoints/eds213-olh-week5-hw-checkpoint.sql
@@ -0,0 +1,14 @@
+--Week 5 - Protect yourself
+--Some strategies were mentioned in class for reducing the possibility of performing UPDATEs and DELETEs that have catastrophic consequences.
+--What strategy will you use?
+
+--Week 5 - Create a trigger
+The Bird_eggs table uniquely identifies each egg by a pair (Nest_ID, Egg_num). The egg numbers for a given nest always have the sequential values 1, 2, 3, 4, etc. For example, there are 3 eggs in nest 14eabaage01:
+
+SELECT * FROM Bird_eggs WHERE Nest_ID = '14eabaage01';
+
+--part 1
+
+--part 2
+
+--part 3
